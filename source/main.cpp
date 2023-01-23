1554a343db6e8ac739a927ca86d21788965643b6
Blooperly <jbrownfisher@gmail.com>
2023-01-22 22:01:11 -0800

    Window running

--- source/main.cpp
+++ source/main.cpp
@@ -3,38 +3,29 @@
 
 using namespace std;
 
-void cmd();
-
 int main(int, char **) {
-    cout << "Hello World!";
-
     SDL_Window* window = nullptr;
     SDL_Surface* screenSurface = nullptr;
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         cout << "SDL Error";
+
     } else {
         if (window = SDL_CreateWindow("Antiquarian", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_SHOWN)) {
-            //Get window surface
             screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x3F, 0xFF, 0x4F ) );
             SDL_UpdateWindowSurface( window );
 
             //Hack to get window to stay up
             SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+            
         } else {
             cout << "SDL Window Error";
         }
-        
     }
     
-    //Destroy window
-    SDL_DestroyWindow( window );
-
-    //Quit SDL subsystems
+    // Shutdown
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
