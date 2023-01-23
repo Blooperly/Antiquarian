d2d2d8426043741813b148c6ca57c16c8ceb0778
Blooperly <jbrownfisher@gmail.com>
2023-01-22 21:42:26 -0800

    Basic windows environment

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include "SDL.h"
 
-int main() {
-    std::cout << "Hello World!";
+using namespace std;
+
+void cmd();
+
+int main(int, char **) {
+    cout << "Hello World!";
+
+    SDL_Window* window = nullptr;
+    SDL_Surface* screenSurface = nullptr;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL Error";
+    } else {
+        if (window = SDL_CreateWindow("Antiquarian", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_SHOWN)) {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+        } else {
+            cout << "SDL Window Error";
+        }
+        
+    }
+    
+    //Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
     return 0;
 }
